574ea925c87613a88a741d62167f09b7d4d22042
giri <gdwiixa@gmail.com>
2023-07-25 00:08:47 +0700

    CRUD laravel

---
+++ resources/views/pages/customers/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.home')
+
+@section('content')
+
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between">
+            <h4 class="mb-0">Semua Data Pelanggan</h4>
+            <a href="{{ route('customers.create') }}" class="btn btn-primary px-3">Tambah Pelanggan Baru</a>
+        </div>
+
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Nama Pelanggan</th>
+                        <th>Email Pelanggan</th>
+                        <th>Nomor Telepon</th>
+                        <th>Alamat Rumah</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($items as $key => $item)
+                        <tr>
+                            <td>{{ ++$key }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->email }}</td>
+                            <td>{{ $item->phone_number }}</td>
+                            <td>{{ $item->address }}</td>
+                            <td>
+                                <div class="d-flex align-items-center gap-2">
+                                    <a href="{{ route('customers.edit', $item->id) }}" class="btn btn-warning">Edit</a>
+                                    <form action="{{ route('customers.destroy', $item->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger" type="submit">Hapus</button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+@endsection
